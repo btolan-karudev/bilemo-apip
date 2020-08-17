2c79deb84f5d8053b543140f3a8561cef39ed6d6
btolan-karudev <beniamin.tolan@outlook.com>
2020-08-17 21:42:05 +0200

    refactor

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
+
 
 /**
  * @ORM\Entity()
